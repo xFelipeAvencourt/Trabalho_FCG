c5ac23cfb3ed6397b77136c2a789c83fc311ad9a
LipT <feelipeavencourt@gmail.com>
2025-11-07 19:15:46 -0300

    Início do projeto com base a partir do laboratório 5.

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/lipt/UFRGS/semestre 7/FCG/Trabalho_FCG/src/glad.c" "CMakeFiles/main.dir/src/glad.c.o" "gcc" "CMakeFiles/main.dir/src/glad.c.o.d"
+  "/home/lipt/UFRGS/semestre 7/FCG/Trabalho_FCG/src/main.cpp" "CMakeFiles/main.dir/src/main.cpp.o" "gcc" "CMakeFiles/main.dir/src/main.cpp.o.d"
+  "/home/lipt/UFRGS/semestre 7/FCG/Trabalho_FCG/src/stb_image.cpp" "CMakeFiles/main.dir/src/stb_image.cpp.o" "gcc" "CMakeFiles/main.dir/src/stb_image.cpp.o.d"
+  "/home/lipt/UFRGS/semestre 7/FCG/Trabalho_FCG/src/textrendering.cpp" "CMakeFiles/main.dir/src/textrendering.cpp.o" "gcc" "CMakeFiles/main.dir/src/textrendering.cpp.o.d"
+  "/home/lipt/UFRGS/semestre 7/FCG/Trabalho_FCG/src/tiny_obj_loader.cpp" "CMakeFiles/main.dir/src/tiny_obj_loader.cpp.o" "gcc" "CMakeFiles/main.dir/src/tiny_obj_loader.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "/home/lipt/UFRGS/semestre 7/FCG/Trabalho_FCG/bin/Linux/main"
+  "/home/lipt/UFRGS/semestre 7/FCG/Trabalho_FCG/bin/Linux/main.pdb"
+  "CMakeFiles/main.dir/src/glad.c.o"
+  "CMakeFiles/main.dir/src/glad.c.o.d"
+  "CMakeFiles/main.dir/src/main.cpp.o"
+  "CMakeFiles/main.dir/src/main.cpp.o.d"
+  "CMakeFiles/main.dir/src/stb_image.cpp.o"
+  "CMakeFiles/main.dir/src/stb_image.cpp.o.d"
+  "CMakeFiles/main.dir/src/textrendering.cpp.o"
+  "CMakeFiles/main.dir/src/textrendering.cpp.o.d"
+  "CMakeFiles/main.dir/src/tiny_obj_loader.cpp.o"
+  "CMakeFiles/main.dir/src/tiny_obj_loader.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
